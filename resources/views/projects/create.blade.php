71f9361d5967019b32f672caf2d1d46c7bb9826c
Qing Ma <gsmofgsm@hotmail.com>
2018-11-03 23:38:47 +0100

    first form handeling - create, store

---
+++ resources/views/projects/create.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+<h1>Create a New Project</h1>
+
+<form action="/projects" method="POST">
+
+    {{ csrf_field() }}
+
+    <div>
+        <input type="text" name="title" placeholder="Project title">
+    </div>
+
+    <div>
+        <textarea name="description" id="description" cols="30" rows="10" placeholder="Project description"></textarea>
+    </div>
+
+    <div>
+        <button type="submit">Create Project</button>
+    </div>
+</form>
+</body>
+</html>
