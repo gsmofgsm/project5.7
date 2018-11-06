d961e82f6b4a04a34013f74c57372f0750ca1565
Qing Ma <gsmofgsm@hotmail.com>
2018-11-06 05:31:08 +0100

    validation rule min character

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -21,8 +21,8 @@ class ProjectsController extends Controller
     public function store()
     {
         request()->validate([
-            'title' => 'required',
-            'description' => 'required',
+            'title' => ['required', 'min:3'],
+            'description' => ['required', 'min:3'],
         ]);
 
         Project::create(request(['title', 'description']));
