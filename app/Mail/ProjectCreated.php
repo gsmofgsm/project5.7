a815d0f74369f5e00f6b92692b0088bf985ac097
Qing Ma <gsmofgsm@hotmail.com>
2018-11-14 19:32:57 +0100

    Mail ProjectCreated

---
+++ app/Mail/ProjectCreated.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Mail;
+
+use App\Project;
+use Illuminate\Bus\Queueable;
+use Illuminate\Mail\Mailable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Contracts\Queue\ShouldQueue;
+
+class ProjectCreated extends Mailable
+{
+    use Queueable, SerializesModels;
+    /**
+     * @var Project
+     */
+    public $project;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct(Project $project)
+    {
+        //
+        $this->project = $project;
+    }
+
+    /**
+     * Build the message.
+     *
+     * @return $this
+     */
+    public function build()
+    {
+        return $this->markdown('mail.project-created');
+    }
+}
