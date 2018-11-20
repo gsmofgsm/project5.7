d039aae2e9b518d48bc3ff20ba061ca9871b95af
Qing Ma <gsmofgsm@hotmail.com>
2018-11-20 19:07:20 +0100

    send mail as side effect: model hook

--- app/Project.php
+++ app/Project.php
@@ -2,11 +2,21 @@
 
 namespace App;
 
+use App\Mail\ProjectCreated;
 use Illuminate\Database\Eloquent\Model;
 
 class Project extends Model
 {
     protected $guarded = [];
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::created(function ($project) {
+            \Mail::to($project->owner->email)->send(new ProjectCreated($project));
+        });
+    }
 
     public function owner()
     {
